cdc70636c1eef45096d8d03435132c6d33371bb9
Roland Weisleder <roland@rweisleder.de>
2023-11-29 12:13:25 +0100

    Initial commit

---
+++ src/main/java/de/rweisleder/demo/config/security/SecurityConfiguration.java
@@ -0,0 +1,37 @@
+package de.rweisleder.demo.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfiguration {
+
+    private final KeycloakLogoutHandler keycloakLogoutHandler;
+
+    public SecurityConfiguration(KeycloakLogoutHandler keycloakLogoutHandler) {
+        this.keycloakLogoutHandler = keycloakLogoutHandler;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/index.html").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(withDefaults()))
+                .logout(logout -> logout
+                        .addLogoutHandler(keycloakLogoutHandler)
+                        .logoutSuccessUrl("/")
+                )
+                .build();
+    }
+
+}
